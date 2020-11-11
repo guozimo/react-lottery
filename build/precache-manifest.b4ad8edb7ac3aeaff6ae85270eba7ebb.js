5fb4a425b6675371d84a2e119cf1c704f2a56de4
郭威东 <915172419@qq.com>
2020-11-11 16:42:26 +0800

    feat: 提交

---
+++ build/precache-manifest.b4ad8edb7ac3aeaff6ae85270eba7ebb.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98b5cf7f474c0b3a312e09ef0e08610d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8c5b54a658091207ae8b",
+    "url": "./static/css/main.3059278c.css"
+  },
+  {
+    "revision": "8c5b54a658091207ae8b",
+    "url": "./static/js/main.ccc34007.js"
+  },
+  {
+    "revision": "986e1e9c89d80231e048e2e20ec3f7a7",
+    "url": "./static/js/main.ccc34007.js.LICENSE"
+  },
+  {
+    "revision": "9746020823a69e4a942db4cdac806317",
+    "url": "./static/media/app-background.97460208.jpg"
+  },
+  {
+    "revision": "7accba1cf0cd8808eab816108e5d6a84",
+    "url": "./static/media/congratulation.7accba1c.png"
+  },
+  {
+    "revision": "d924ad1187e642e1298ff283f79df9c4",
+    "url": "./static/media/hksn.d924ad11.TTF"
+  },
+  {
+    "revision": "3960d0f45e891b4521a256a132ddb539",
+    "url": "./static/media/id_.3960d0f4.png"
+  },
+  {
+    "revision": "068257c3110735b9f9bc62778ec53824",
+    "url": "./static/media/id_0.068257c3.png"
+  },
+  {
+    "revision": "ea374b419ddfd8f1354be71f54a54930",
+    "url": "./static/media/id_1.ea374b41.png"
+  },
+  {
+    "revision": "f394c22fcc0d40561bd2c14908121684",
+    "url": "./static/media/id_2.f394c22f.png"
+  },
+  {
+    "revision": "0c7ca490ebb2950ffd0a50e3c39e1bc5",
+    "url": "./static/media/id_3.0c7ca490.png"
+  },
+  {
+    "revision": "e20723d3e761284550cf7fb32ce99c24",
+    "url": "./static/media/id_4.e20723d3.png"
+  },
+  {
+    "revision": "da82ea4f1bc1a37d5540f7eba72cf019",
+    "url": "./static/media/nose_down.da82ea4f.png"
+  },
+  {
+    "revision": "f6ac80d5ce125ce7be2d0ca5f15a391b",
+    "url": "./static/media/nose_up.f6ac80d5.png"
+  },
+  {
+    "revision": "0fc4df1ba89e5ed60ba8c400ddd3af42",
+    "url": "./static/media/pig.0fc4df1b.png"
+  },
+  {
+    "revision": "d5cf98b8c2229682f020b7e3dd35ebf1",
+    "url": "./static/media/title.d5cf98b8.png"
+  },
+  {
+    "revision": "cc1402449fba6d489e21c7028ce9e271",
+    "url": "./static/media/title_0.cc140244.png"
+  },
+  {
+    "revision": "63abd683099ae51fdbc8813c1e1c41f9",
+    "url": "./static/media/title_1.63abd683.png"
+  },
+  {
+    "revision": "1b324741cc40ac8d0496e838c024a725",
+    "url": "./static/media/title_2.1b324741.png"
+  },
+  {
+    "revision": "9ab7e2f45e4eb3d8d162f34632a892a3",
+    "url": "./static/media/title_3.9ab7e2f4.png"
+  },
+  {
+    "revision": "d32999b105ac32bd081975a68c375418",
+    "url": "./static/media/title_4.d32999b1.png"
+  }
+]);
---
+++ build/service-worker.js
@@ -0,0 +1,24 @@
+importScripts("./precache-manifest.b4ad8edb7ac3aeaff6ae85270eba7ebb.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/* eslint-disable */
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("./index.html", {
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
+
+self.addEventListener('message', event => {
+  if (event.data === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
+
